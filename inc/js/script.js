6d619540368443895a4e007f7cf4f4490fbe2a5e
kang2oon <kang2oon@gmail.com>
2018-02-14 16:07:49 +0900

    join start

--- inc/js/script.js
+++ inc/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+	var cur_path = window.location.pathname;
+	if (cur_path == '/') {
+		$('body').addClass('mvb_show');
+	}else{
+		$('body').removeClass('mvb_show');
+		$('.gnb li').removeClass('on');
+	}
+	
 	/* GNB Action */
 	$('.gnb .program').on('click mouseenter mouseover', function(){
 		$('.menu_over').slideDown();
@@ -15,4 +23,23 @@ $(document).ready(function(){
 	$('.plan').tabslet({
 		animation: true
 	});
+	sel_lst();
+	
+	$('.modal').iziModal({
+		headerColor: '#f15a21',
+		overlayColor: 'rgba(0, 0, 0, 0.7)',
+		radius: 8,
+		borderBottom: false,
+		zindex: 5000,
+		onOpened: function(event){ // 이 달의 생활습관표 탭 재구동
+			var lstw = $('.lifestyle_table');
+			lstw.on('mouseenter mouseover', function(){
+				sel_lst();
+			});
+		}
+	});
 });
+/* 이 달의 생활습관표 */
+function sel_lst(){
+	$('.lifestyle_table').tabslet();
+}
